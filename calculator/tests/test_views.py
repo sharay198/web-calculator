2b4170a4efa4fefc5d613ce2d6f5cbd805b3c274
Sergey_Sharay <sergeysharay1987@gmail.com>
2020-11-10 22:17:49 +0300

    Install pytest-django and test have become passed

--- calculator/tests/test_views.py
+++ calculator/tests/test_views.py
@@ -1,43 +1,19 @@
-import os
-import sys
-
-from django.conf import settings
-from django.http import request
 from django.urls import reverse, resolve
-from django.test import RequestFactory
-from django.shortcuts import render
+
 # Create your tests here.
-import pytest
 
-#from calculator.forms import ExpForm
-#from calculator.views import get_exp
-
-
-# class TestVIews:
-#
-#     def test_views_get_exp_post(self):
-#         d = {'exp': '1-1', 'result_of_exp': 0}
-#         bound_form = ExpForm(d)
-#         assert get_exp(request) == render(request, 'calculator/index.html', {'form': bound_form})
-
-# def test_index_url(self):
-#     path = reverse('index')
-#     assert resolve(path).url_name == 'index'
-#     print(path)
-#     print(resolve(path).view_name)
-#     print(resolve(path).func)
 
 
 # class TestUrls:
-settings.configure()
+# settings.configure()
+
 
 def test_index_url():
     path = reverse('index')
-    # print(resolve(path).view_name)
+    #print(resolve(path).view_name)
     assert resolve(path).view_name == 'index'
 
-
-# def test_exp_list_url():
-#     path = reverse('database')
-#     # print(resolve(path).view_name)
-#     assert resolve(path).view_name == 'database'
+def test_exp_list_url():
+    path = reverse('database')
+    # print(resolve(path).view_name)
+    assert resolve(path).view_name == 'database'
