da6f22c14ce8e72ea3b40c2a87c710ae6e3c26c3
Sergey_Sharay <sergeysharay1987@gmail.com>
2021-02-07 16:59:19 +0300

    Removing class TestUrls and it's methods in test_urls

--- calculator/tests/test_urls.py
+++ calculator/tests/test_urls.py
@@ -11,25 +11,3 @@ def test_path_of_page(view_name, id):
         path = reverse(view_name, kwargs={'id': id})
     assert resolve(path).view_name == view_name
 
-
-class TestUrls:
-
-    def test_path_of_index_page(self):
-        path = reverse('index')
-        # print(resolve(path).view_name)
-        assert resolve(path).view_name == 'index'
-
-    def test_path_of_database_page(self):
-        path = reverse('database')
-        print(resolve(path).view_name)
-        assert resolve(path).view_name == 'database'
-
-    def test_path_of_detail_page(self):
-        path = reverse('details', kwargs={'id': 5})
-        print(resolve(path).view_name)
-        assert resolve(path).view_name == 'details'
-
-    def test_path_of_delete_page(self):
-        path = reverse('delete', kwargs={'id': 5})
-        print(resolve(path).view_name)
-        assert resolve(path).view_name == 'delete'
