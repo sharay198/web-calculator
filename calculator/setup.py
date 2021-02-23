80accd407f1e9df099a919a819e78d9f53421e05
Sergey_Sharay <sergeysharay1987@gmail.com>
2021-02-23 18:32:29 +0300

    Adding setup.py

---
+++ calculator/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(name='web_calculator',
+      version='0.0.1',
+      author='Example Author',
+      author_email='author@example.com',
+      description='A small example package',
+      long_description='The application web_calculator allow you to calculate expressions, using five operators "+", "-", "*", "**"',
+      long_description_content_type='text/markdown',
+      url='https://github.com/sergeysharay1987/web_calculator/tree/Remake_validation',
+      packages = find_packages()
+      )
+
