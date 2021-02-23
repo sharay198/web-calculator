4b2546d1ac5cdd9c473342de4972e4baab3eed91
Sergey_Sharay <sergeysharay1987@gmail.com>
2021-02-23 19:36:48 +0300

    Adding information in setup.py

--- calculator/setup.py
+++ calculator/setup.py
@@ -3,11 +3,12 @@ from setuptools import setup, find_packages
 
 setup(name='web_calculator',
       version='0.0.1',
-      author='Example Author',
-      author_email='author@example.com',
-      description='A small example package',
-      long_description='The application web_calculator allow you to calculate expressions, using five operators "+", "-", "*", "**"',
-      long_description_content_type='text/markdown',
+      author='Sergey Sharay',
+      author_email='sergeysharay1987@gmail.com',
+      description='A simple online calculator',
+      long_description='The application web_calculator allow you to calculate expressions, using five operators "+", '
+                       '"-", "*", "/", "**"',
+      long_description_content_type='text/restructuredtext',
       url='https://github.com/sergeysharay1987/web_calculator/tree/Remake_validation',
       packages = find_packages()
       )
