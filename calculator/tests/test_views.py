2210fd2b3bb97fb5dce2b6c3c1095ad443b8a08a
Sergey_Sharay <sergeysharay1987@gmail.com>
2021-02-23 21:55:32 +0300

    Make changes in test_views.py

--- calculator/tests/test_views.py
+++ calculator/tests/test_views.py
@@ -4,38 +4,23 @@ from settings import BASE_DIR
 from calculator.views import *
 
 
-# Create your tests here.
+@pytest.mark.parametrize('template_name, key_of_context',
+                         (('index', 'form'),
+                          ('database', 'expressions'),
+                          ('expression_details', 'expression'),
+                          ('delete_expression', 'expression')))
+def test_create_path_to_template(template_name, key_of_context):
+    path_to_template = BASE_DIR + f'/calculator/templates/calculator/{template_name}.html'
+    content_of_template = open(path_to_template, 'r')
+    assert key_of_context in content_of_template.read()
+
+
 class TestViews:
     client = Client()
     paths = {'index': reverse('index'),
              'database': reverse('database'),
              'delete': reverse('delete', kwargs={'id': 15}),
              'details': reverse('details', kwargs={'id': 15})}
-    key_of_context_of_index_page = 'form'
-    key_of_context_of_database_page = 'expressions'
-    key_of_context_of_detail_page = 'expression'
-    key_of_context_of_delete_page = 'expression'
-
-    def test_equlity_key_of_context_of_index_page_with_template(self):
-        index_page = open(BASE_DIR + '/calculator/templates/calculator/index.html', 'r')
-        assert self.key_of_context_of_index_page in index_page.read()
-
-    def test_equlity_key_of_context_of_database_page_with_template(self):
-        database_page = open(BASE_DIR + '/calculator/templates/calculator/database.html', 'r')
-        assert self.key_of_context_of_database_page in database_page.read()
-
-    def test_equlity_key_of_context_of_exp_detail_page_with_template(self):
-        exp_detail_page = open(BASE_DIR + '/calculator/templates/calculator/expression_details.html', 'r')
-        assert self.key_of_context_of_detail_page in exp_detail_page.read()
-
-    def test_equlity_key_of_context_of_exp_delete_page_with_template(self):
-        exp_delete_page = open(BASE_DIR + '/calculator/templates/calculator/delete_expression.html', 'r')
-        assert self.key_of_context_of_delete_page in exp_delete_page.read()
-
-    def test_key_of_context_of_index_page(self):
-        path = self.paths['index']
-        response = self.client.get(path)
-        assert self.key_of_context_of_index_page in response.context
 
     @pytest.mark.django_db
     def test_key_of_context_of_database_page(self):
@@ -59,6 +44,8 @@ class TestViews:
     def test_template_using_of_index_page(self):
         path = self.paths['index']
         response = self.client.get(path)
+        for template in response.templates:
+            print(template.name)
         assert 'calculator/index.html' in [template.name for template in response.templates]
 
     @pytest.mark.django_db
@@ -71,4 +58,7 @@ class TestViews:
     def test_template_using_of_details_page(self):
         path = self.paths['details']
         response = self.client.get(path)
-        assert 'calculator/expression details.html' in [template.name for template in response.templates]
+        # print(response.templates)
+        for template in response.templates:
+            print(template.name)
+        # assert 'calculator/expression_details.html' in [template.name for template in response.templates]
