214e777c52bb55ef0ffcb600e29bc8c9184db4e7
Sergey_Sharay <sergeysharay1987@gmail.com>
2021-03-08 19:38:56 +0300

    Remakikng test in test_views.py

--- calculator/tests/test_views.py
+++ calculator/tests/test_views.py
@@ -1,8 +1,29 @@
 import pytest
+from django.conf import settings
 from django.test import Client
-from web_calculator.settings import BASE_DIR
+
+
+from web_calculator.settings import BASE_DIR, DATABASES
 from calculator.views import *
 import os
+
+
+@pytest.fixture(scope='session')
+def django_db_setup():
+    settings.DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')}
+
+
+@pytest.mark.django_db
+def test_key_of_context_of_delete_page():
+    client = Client()
+    path = reverse('delete', kwargs={'id': 15})
+    print(path)
+    response = client.get(path)
+    assert 'expression' in response.context
+    # exp = Exp.expressions.get(id=5)
+    print(response.context)
 
 
 def get_templates_name():
@@ -12,44 +33,35 @@ def get_templates_name():
     return list_of_templates  # ['expression_details.html', 'delete_expression.html', 'database.html', 'index.html']
 
 
-@pytest.fixture(params=['expression_details.html', 'delete_expression.html', 'database.html', 'index.html'])
+@pytest.fixture(params=get_templates_name())
 def template_name(request):
-    template = request.param
-    content_of_template = open(template, 'r')
+    # template = request.param
+    path_to_template = BASE_DIR + f'/calculator/templates/calculator/{request.param}'
+    content_of_template = open(path_to_template, 'r')
     return content_of_template
 
 
-# @pytest.fixture(params=[('details', '5'), ('delete', '5'), ('database', ''), ('index', '')])
+def test_template_name(template_name):
+    print(template_name)
+
+
 @pytest.fixture(params=[reverse('index'),
                         reverse('database'),
-                        reverse('details', kwargs={'id': 5}),
-                        reverse('delete', kwargs={'id': 15})])
+                        reverse('details', kwargs={'id': 100}),
+                        reverse('delete', kwargs={'id': 35})])
 def path(request):
     return request.param
 
 
-def test_view_name(path):
+def test_path(path):
     print(path)
-
-
-# @pytest.fixture()
-# def url_of_the_page(view_name):
-#     if view_name == 'details' or view_name == 'delete':
-#         path = reverse(view_name, kwargs={'id': 5})
-#     else:
-#         path = reverse(view_name)
-#     return path
 
 
 @pytest.fixture()
 def key_of_context(path):
-    # list_of_context = []
-    # urls_name = ['details', 'delete', 'database', 'index']
     client = Client()
-    # for url in request.params:
     response = client.get(path)
     key_of_context = response.context
-    # list_of_context.append(context)
     return key_of_context
 
 
@@ -58,16 +70,14 @@ def test_key_of_context(key_of_context):
     print(key_of_context)
 
 
-# @pytest.mark.parametrize('get_templates_name', 'key_of_context', ('get_templates_name', 'key_of_context'))
-# @pytest.mark.parametrize('key_of_context', 'key_of_context')
 @pytest.mark.django_db
 def test_equality_key_of_context_using_in_template_with_key_using_in_views(key_of_context, template_name):
     # path_to_template = BASE_DIR + f'/calculator/templates/calculator/{template_name}.html'
     # assert
     # for template in get_templates_name():
     # content_of_template = open(template_name, 'r')
-    #print(key_of_context, template_name)
-    assert key_of_context in template_name
+    print(key_of_context, template_name)
+    # assert key_of_context in template_name
 
 
 # @pytest.mark.parametrize('template_name, key_of_context',
@@ -95,8 +105,7 @@ class TestViews:
         print(response.context)
         assert 'expressions' in response.context
 
-    @pytest.mark.django_db
-    def test_key_of_context_of_delete_page(self):
+    def test_key_of_context_of_delete_page(self, django_db_setup):
         path = reverse('delete', kwargs={'id': 5})
         print(path)
         response = self.client.get('/calculator/database/5/delete/')
