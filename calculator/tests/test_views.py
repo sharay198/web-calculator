cc0a98b032c85502565873ff78f69dd8294b06f4
sergeysharay1987 <SergeySharay1987@gmail.com>
2021-08-09 00:08:15 +0300

    Some changes in test_views.py

--- calculator/tests/test_views.py
+++ calculator/tests/test_views.py
@@ -3,18 +3,10 @@ from django.core.management import call_command
 from django.test.client import Client
 from web_calculator.settings import BASE_DIR
 from calculator.views import *
+# from pytest_django.asserts
 
-_keys_of_context_using_in_views = ['expression', 'expression', 'expressions', 'form']
-
-# @pytest.fixture(scope='session')
-# def django_db_setup():
-#     settings.DATABASES['default'] = {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')}
-
-
-
-
+# define variable containing names of context's keys in views.py
+_keys_of_context_using_in_views = ['form', 'expression', 'expressions', 'expression']
 
 
 @pytest.fixture(scope='session')
@@ -23,8 +15,8 @@ def django_db_setup(django_db_setup, django_db_blocker):
         call_command('loaddata', BASE_DIR +'/db_for_tests.json')
 
 
-@pytest.fixture(params=[reverse('details', kwargs={'id': 1}),  # /calculator/database/100/
-                        reverse('delete', kwargs={'id': 3}),  # /calculator/database/35/delete/
+@pytest.fixture(params=[reverse('details', kwargs={'id': 100}),  # /calculator/database/100/
+                        reverse('delete', kwargs={'id': 35}),  # /calculator/database/35/delete/
                         reverse('database'),  # /calculator/database
                         reverse('index')])  # /calculator/
 def make_response(request):
@@ -48,7 +40,7 @@ def test_path():
     # exp_details = Expression.objects.get(id=id)
     client = Client()
     response = client.get(path)
-    print(response)
+    print(response.context.keys())
     # print(path)
     # print(id_from_path)
     # print(exp_details)
@@ -67,13 +59,15 @@ def key_of_context(make_response):
 
 @pytest.mark.django_db
 def test_key_of_context(make_response):
+    content = make_response.content
     context_list = make_response.context
-    print(context_list)
-    # keys = context_list.keys()
-    # for key in keys:
-    #
-    #     if key in _keys_of_context_using_in_views:
-    #         return key
+    #print(context_list)
+    keys = context_list.keys()
+    print(keys)
+    for key in keys:
+
+        if key in _keys_of_context_using_in_views:
+            return key
 
 
 @pytest.fixture()
@@ -87,5 +81,5 @@ def template(make_response):
 
 @pytest.mark.django_db
 def test_equality_key_of_context_using_in_template_with_key_using_in_views(template, key_of_context):
-    """Check if key of context in views.py equal key using in template"""
+    """Check if key of context in views.py equals key using in template"""
     assert key_of_context in template
