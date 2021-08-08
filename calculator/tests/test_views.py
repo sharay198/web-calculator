d75680d97c3dbab8f65741bb10f34c125398b01b
sergeysharay1987 <SergeySharay1987@gmail.com>
2021-08-08 22:29:18 +0300

    Add full path to 'db_for_tests.json'

--- calculator/tests/test_views.py
+++ calculator/tests/test_views.py
@@ -1,12 +1,10 @@
 import pytest
-from django.conf import settings
+from django.core.management import call_command
 from django.test.client import Client
 from web_calculator.settings import BASE_DIR
 from calculator.views import *
-import os
 
 _keys_of_context_using_in_views = ['expression', 'expression', 'expressions', 'form']
-
 
 # @pytest.fixture(scope='session')
 # def django_db_setup():
@@ -15,8 +13,18 @@ _keys_of_context_using_in_views = ['expression', 'expression', 'expressions', 'f
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')}
 
 
-@pytest.fixture(params=[reverse('details', kwargs={'id': 100}),  # /calculator/database/100/
-                        reverse('delete', kwargs={'id': 35}),  # /calculator/database/35/delete/
+
+
+
+
+@pytest.fixture(scope='session')
+def django_db_setup(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        call_command('loaddata', BASE_DIR +'/db_for_tests.json')
+
+
+@pytest.fixture(params=[reverse('details', kwargs={'id': 1}),  # /calculator/database/100/
+                        reverse('delete', kwargs={'id': 3}),  # /calculator/database/35/delete/
                         reverse('database'),  # /calculator/database
                         reverse('index')])  # /calculator/
 def make_response(request):
@@ -24,6 +32,27 @@ def make_response(request):
     client = Client()
     response = client.get(path)
     return response
+
+
+def get_id_from_path(path):
+    id_from_path = path.split('/')
+    id = id_from_path[-2]
+    return id
+
+
+@pytest.mark.django_db()
+def test_path():
+    path = reverse('details', kwargs={'id': 1})
+
+    # id = get_id_from_path(path)
+    # exp_details = Expression.objects.get(id=id)
+    client = Client()
+    response = client.get(path)
+    print(response)
+    # print(path)
+    # print(id_from_path)
+    # print(exp_details)
+    # print(response.context)
 
 
 @pytest.fixture()
@@ -34,6 +63,17 @@ def key_of_context(make_response):
 
         if key in _keys_of_context_using_in_views:
             return key
+
+
+@pytest.mark.django_db
+def test_key_of_context(make_response):
+    context_list = make_response.context
+    print(context_list)
+    # keys = context_list.keys()
+    # for key in keys:
+    #
+    #     if key in _keys_of_context_using_in_views:
+    #         return key
 
 
 @pytest.fixture()
