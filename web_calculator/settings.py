8e3d316ef578bb45a5ab868df5db79f89df49e2f
sergeysharay1987 <SergeySharay1987@gmail.com>
2021-08-15 00:03:09 +0300

    Changes variable DATABASES to connect postgresql

--- web_calculator/settings.py
+++ web_calculator/settings.py
@@ -79,10 +79,20 @@ WSGI_APPLICATION = 'web_calculator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_postgresql',
+        'USER': 'postgresuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
