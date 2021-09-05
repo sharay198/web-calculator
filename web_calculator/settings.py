02695e7035600e6fff106bf08beba98dc6e251e9
sergeysharay1987 <SergeySharay1987@gmail.com>
2021-09-05 22:08:31 +0300

    Some changes in files

--- web_calculator/settings.py
+++ web_calculator/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         # 'HOST': 'db',
         'PORT': 5432,
     }
